c7225d01a01f10e33bc2c29de404de0a17a515c5
Pelle Bjerkestrand <pelle@example.com>
2018-05-23 21:23:34 +0200

    fixes attribute name.

--- source/app.jsx
+++ source/app.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const App = ({ children, css, js }) => (
   <html>
     <head>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <title>Pelle Bjerkestrand - Programmer</title>
       {css.map(file => <link key={file} rel="stylesheet" href={`/${file}`} />)}
     </head>
